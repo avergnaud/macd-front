12c31f36495b9c04f0067382b32d037f7077eac5
Adrien Vergnaud <adrien@MacBook-Air-de-Adrien.local>
2020-04-27 20:42:22 +0200

    ohlc macd in one graph

--- src/components/ohlcmacd/OhlcMacdChart/OhlcMacdChart.tsx
+++ src/components/ohlcmacd/OhlcMacdChart/OhlcMacdChart.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import OhlcMacdChartProps from "../OhlcMacdChartProps";
-
+import { OhlcMacdChartD3 } from './ohlc-macd-chart-d3';
 
 export const OhlcMacdChart = (props: OhlcMacdChartProps) => {
+
+    const d3Container = useRef(null);
+
+    const chart = new OhlcMacdChartD3({
+        element: d3Container.current,
+        config: props.visual
+      });
+
+    useEffect(() => {
+        
+        if (props && props.data && props.data.length > 0 && d3Container.current) {
+            
+            chart.setElement(d3Container.current);
+            chart.draw(props.data);
+        }
+    }, [props, chart/*, d3Container.current*/]);
     
-    return <div>Hi</div>
+    return <div ref={d3Container}></div>;
 }
